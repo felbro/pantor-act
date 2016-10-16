f8c4b82c6ecce544d49143c8d0669f9778021958
Felix Broberg <felbro@kth.se>
2016-10-16 18:34:29 +0200

    made some prog on onOrderReplaced

--- solutions/md2.cc
+++ solutions/md2.cc
@@ -7,6 +7,7 @@
 #include <map>
 #include <algorithm>
 #include <vector>
+#include <functional>
 
 using namespace ACT;
 
@@ -48,6 +49,13 @@ struct StatsObserver : public MsgObserver
 
 
         Encoder enc;
+
+        static bool greater(u64 a,u64 b){
+                return a>b;
+        }
+        static bool lesser(u64 a,u64 b){
+                return a<b;
+        }
 
 
         StatsObserver () {
@@ -101,38 +109,23 @@ struct StatsObserver : public MsgObserver
 
                 OS [m.serverOrderId] = m;
 
-                if(m.side == Side::Buy) {
+                if(m.side == Side::Buy)
+                        onInserted(m, IObuy [m.instrumentId], IS [m.instrumentId].bidPrice, IS [m.instrumentId].bidQuantity,lesser);
 
-                        if ((IS [m.instrumentId].bidPrice < m.price) || IObuy[m.instrumentId].empty()) {
-                                IS [m.instrumentId].bidPrice = m.price;
-                                IS [m.instrumentId].bidQuantity = m.quantity;
-
-                        }
-                        else if (IS [m.instrumentId].bidPrice == m.price ) {
-                                IS [m.instrumentId].bidQuantity += m.quantity;
-
-                        }
-                        IObuy [m.instrumentId].push_back(m.serverOrderId);
-                        enc.send (IS [m.instrumentId]);
+                else
+                        onInserted(m, IOsell [m.instrumentId], IS [m.instrumentId].askPrice, IS [m.instrumentId].askQuantity,greater);
+        }
+        void onInserted(Public::OrderInserted m, std::vector<u64> & vec, u64 & p, u64 & q,std::function<bool (u64,u64)> func){
+                if (func(p,m.price) || vec.empty()) {
+                        p = m.price;
+                        q = m.quantity;
 
                 }
+                else if (p == m.price )
+                        q += m.quantity;
 
-                else {
-
-                        if((IS [m.instrumentId].askPrice > m.price) || IOsell[m.instrumentId].empty()) {
-                                IS [m.instrumentId].askPrice = m.price;
-                                IS [m.instrumentId].askQuantity = m.quantity;
-
-                        }
-                        else if (IS [m.instrumentId].askPrice == m.price) {
-                                IS [m.instrumentId].askQuantity += m.quantity;
-
-                        }
-                        IOsell [m.instrumentId].push_back(m.serverOrderId);
-                        enc.send (IS [m.instrumentId]);
-
-                }
-
+                vec.push_back(m.serverOrderId);
+                enc.send (IS [m.instrumentId]);
         }
 
 
@@ -144,6 +137,12 @@ struct StatsObserver : public MsgObserver
                 {
                         std::cerr << "Error: Unknown order: " << m.toString() << std::endl;
                         abort ();
+                }
+                if (OS[m.serverOrderId].instrumentId != m.instrumentId) {
+                        std::cerr << "Error: Instrument Info for given order does not correspond with message Instrument Info: " <<
+                        m.toString() << std::endl;
+                        std::cerr << OS[m.serverOrderId].instrumentId << " != " << m.instrumentId << std::endl;
+                        abort();
                 }
 
                 auto sortbyPriceDesc = [&](const u64 lhs,const u64 rhs)->bool {
@@ -274,7 +273,73 @@ struct StatsObserver : public MsgObserver
 
 
         void onOrderReplaced (const Public::OrderReplaced & m ) override {
+                // error messages
+                const auto it = OS.find (m.serverOrderId);
+                if (it == OS.end ())
+                {
+                        std::cerr << "Error: Unknown order: " << m.toString() << std::endl;
+                        abort ();
+                }
+                if (OS[m.serverOrderId].instrumentId != m.instrumentId) {
+                        std::cerr << "Error: Instrument Info for given order does not correspond with message Instrument Info: " <<
+                        m.toString() << std::endl;
+                        std::cerr << OS[m.serverOrderId].instrumentId << " != " << m.instrumentId << std::endl;
+                        abort();
+                }
 
+                auto sortbyPriceDesc = [&](const u64 lhs,const u64 rhs)->bool {
+
+                        return OS[lhs].price > OS[rhs].price;
+                };
+
+                auto sortbyPriceAsc = [&](const u64 lhs,const u64 rhs)->bool {
+
+                        return OS[lhs].price < OS[rhs].price;
+                };
+
+                OS[m.newServerOrderId] = OS[m.serverOrderId];
+                OS[m.newServerOrderId].price = m.price;
+                OS[m.newServerOrderId].quantity = m.quantity;
+//                IS[m.instrumentId] = m.newServerOrderId;
+
+                if (OS [m.serverOrderId].side == Side::Buy) {
+
+                        sort(IObuy [m.instrumentId].begin(),IObuy [m.instrumentId].end(),sortbyPriceDesc);
+                        toReplace(m, IObuy [m.instrumentId], IS [m.instrumentId].bidPrice, IS [m.instrumentId].bidQuantity,lesser);
+                }
+                /*   else{
+                        sort(IOsell [m.instrumentId].begin(),IOsell [m.instrumentId].end(),sortbyPriceAsc);
+                        toExecute(m, IOsell [m.instrumentId], IS [m.instrumentId].askPrice, IS [m.instrumentId].askQuantity);
+                   }*/
+                OS.erase(m.serverOrderId);
+
+        }
+
+        void toReplace(Public::OrderReplaced m, std::vector<u64> & vec, u64 & p, u64 & q,std::function<bool (u64,u64)> func){
+
+                if (func(p,m.price)) {
+                        p = m.price;
+                        q = m.quantity;
+                        for (unsigned i = 0; i < vec.size(); i++) {
+                                if (OS[vec[i]].serverOrderId == m.serverOrderId) {
+                                        vec.erase(vec.begin()+i);
+                                        break;
+                                }
+                        }
+
+                }
+
+                else if (m.price == p) {
+                        q -= OS[m.serverOrderId].quantity;
+                        q += m.quantity;
+
+                        if(q == 0) decreaseAndReplace(vec,p,q);
+                        vec.erase(vec.begin());
+
+                        enc.send(IS [m.instrumentId]);
+                }
+//HÖR MÅSTE NGT GÖRAS
+                vec.push_back(m.newServerOrderId);
         }
 
 
